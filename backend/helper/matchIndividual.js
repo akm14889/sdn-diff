4e27109e90d7fe6457e3de607db38e7f468f8633
ananya-mallik-ps <ananya.mallik@paymentsense.com>
2023-03-12 12:46:00 +0000

    Add EU SDN list matching

--- backend/helper/matchIndividual.js
+++ backend/helper/matchIndividual.js
@@ -36,7 +36,8 @@ export async function missingPersonInUKList() {
 
     for (const oSanctionedPerson of oUKSDNData) {
         let personMissing = true;
-        if (oSanctionedPerson.Names.Name && Array.isArray(oSanctionedPerson.Names.Name)) {
+        if (oSanctionedPerson.Names.Name &&
+            Array.isArray(oSanctionedPerson.Names.Name)) {
             if (personMissing === false) {
                 break;
             }
@@ -44,15 +45,14 @@ export async function missingPersonInUKList() {
                 if (personMissing === false) {
                     break;
                 }
-                if (oSanctionedPersonName.NameType.toLowerCase() === "primary name") {
-                    let nameInUkList = oSanctionedPersonName.Name6 &&
-                        oSanctionedPersonName.Name6.toLowerCase();
-                    for(const oPerson of oPelicanData) {
-                        const oPersonName = oPerson.Entity && oPerson.Entity.toLowerCase();
-                        if (oPersonName && oPersonName.indexOf(nameInUkList) != -1) {
-                            personMissing = false;
-                            break;
-                        }
+                let nameInUkList = "";
+                nameInUkList = updateName(nameInUkList, oSanctionedPersonName);
+                nameInUkList = nameInUkList.toLowerCase();
+                for(const oPerson of oPelicanData) {
+                    const oPersonName = oPerson.Entity && oPerson.Entity.toLowerCase();
+                    if (oPersonName && oPersonName.indexOf(nameInUkList) != -1) {
+                        personMissing = false;
+                        break;
                     }
                 }
             }
@@ -61,11 +61,12 @@ export async function missingPersonInUKList() {
             /**
              * @todo find which name to pick picking Name6 for now
              */
-            if (oSanctionedPerson.Names.Name.NameType && oSanctionedPerson.Names.Name.NameType.toLowerCase() === "primary name") {
-                let nameInUkList = oSanctionedPerson.Names.Name &&
-                    oSanctionedPerson.Names.Name.Name6 &&
-                    oSanctionedPerson.Names.Name.Name6.toLowerCase();
-
+            if (oSanctionedPerson.Names.Name &&
+                oSanctionedPerson.Names.Name.NameType &&
+                oSanctionedPerson.Names.Name.NameType.toLowerCase() === "primary name") {
+                let nameInUkList = "";
+                nameInUkList = updateName(nameInUkList, oSanctionedPerson.Names.Name);
+                nameInUkList = nameInUkList.toLowerCase();
                 for(const oPerson of oPelicanData) {
                     const oPersonName = oPerson.Entity && oPerson.Entity.toLowerCase();
                     if (oPersonName && oPersonName.indexOf(nameInUkList) != -1) {
@@ -107,6 +108,34 @@ export async function missingPersonInUsList() {
                 break;
             }
         }
+        let oScantionedPersonAliases = oSanctionedPerson.akaList &&
+            oSanctionedPerson.akaList.aka;
+        if (oScantionedPersonAliases) {
+            if (Array.isArray(oScantionedPersonAliases)) {
+                for (const alias of oScantionedPersonAliases ) {
+                    if (personMissing === false) {
+                        break;
+                    }
+                    let oSanctionedPersonAliasName = alias.lastName && alias.lastName.toLowerCase();
+                    for(const oPerson of oPelicanData) {
+                        const oPersonName = oPerson.Entity && oPerson.Entity.toLowerCase();
+                        if (oPersonName && oPersonName.indexOf(oSanctionedPersonAliasName) != -1) {
+                            personMissing = false;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                let oSanctionedPersonAliasName = oScantionedPersonAliases.lastName && oScantionedPersonAliases.lastName.toLowerCase();
+                for(const oPerson of oPelicanData) {
+                    const oPersonName = oPerson.Entity && oPerson.Entity.toLowerCase();
+                    if (oPersonName && oPersonName.indexOf(oSanctionedPersonAliasName) != -1) {
+                        personMissing = false;
+                        break;
+                    }
+                }
+            }
+        }
         if (personMissing) {
             diffData.push(oSanctionedPerson);
         }
@@ -143,8 +172,11 @@ export async function missingPersonInUnList() {
         if (oSanctionedPerson && oSanctionedPerson.THIRD_NAME) {
             oSanctionedPersonName += " " + oSanctionedPerson.THIRD_NAME;
         }
-        oSanctionedPersonName = oSanctionedPersonName && oSanctionedPersonName.toLowerCase();
+        if (oSanctionedPerson && oSanctionedPerson.FOURTH_NAME) {
+            oSanctionedPersonName += " " + oSanctionedPerson.FOURTH_NAME;
+        }
 
+        oSanctionedPersonName = oSanctionedPersonName && oSanctionedPersonName.toLowerCase();
         for(const oPerson of oPelicanData) {
             const oPersonName = oPerson.Entity && oPerson.Entity.toLowerCase();
             if (oPersonName && oPersonName.indexOf(oSanctionedPersonName) != -1) {
@@ -165,6 +197,85 @@ export async function missingPersonInUnList() {
     console.log("|    Saved missing entity/person from UN list     \u{1F4BE} \u{1F4BE}                      |");
 }
 
-export async  function missingPersonInEUList() {
+export async  function missingPersonInEuList() {
+    console.log("|    Getting missing entity/person from EU list   \u{1F973} \u{1F973}                      |");
+    printWait();
+    let oEUSDNData = await fs.readFile(ENV.DATA_SOURCE.EU.FILE, "utf8");
+    oEUSDNData = JSON.parse(oEUSDNData);
+    const diffData = [];
+    oEUSDNData = oEUSDNData &&
+        oEUSDNData.export &&
+        oEUSDNData.export.sanctionEntity;
 
+    for ( const oSanctionedPerson of oEUSDNData ) {
+        let personMissing = true;
+        let oSanctionedPersonNameAliases = oSanctionedPerson.nameAlias;
+        if (oSanctionedPersonNameAliases &&
+            Array.isArray(oSanctionedPersonNameAliases)) {
+            for(const oSanctionedPersonName of oSanctionedPersonNameAliases) {
+                if (personMissing === false) {
+                    break;
+                }
+                let oScantionPersonName = oSanctionedPersonName["$"];
+                oScantionPersonName = oScantionPersonName.wholeName.toLowerCase();
+                for(const oPerson of oPelicanData) {
+                    const oPersonName = oPerson.Entity && oPerson.Entity.toLowerCase();
+                    if (oPersonName && oPersonName.indexOf(oScantionPersonName) != -1) {
+                        personMissing = false;
+                        break;
+                    }
+                }
+            }
+        } else if (oSanctionedPersonNameAliases) {
+            let oScantionPersonName = oSanctionedPersonNameAliases["$"];
+            oScantionPersonName = oScantionPersonName.wholeName.toLowerCase();
+            for(const oPerson of oPelicanData) {
+                const oPersonName = oPerson.Entity && oPerson.Entity.toLowerCase();
+                if (oPersonName && oPersonName.indexOf(oScantionPersonName) != -1) {
+                    personMissing = false;
+                    break;
+                }
+            }
+        }
+        if (personMissing) {
+            diffData.push(oSanctionedPerson);
+        }
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    stopPrintWait();
+    console.log("|    Diff size:            " + diffData.length + "                   \u{1F4D6} \u{1F4D6}                      |");
+    console.log("|    Full List size:       " + oEUSDNData.length + "                   \u{1F4D5} \u{1F4D5}                      |");
+    const fileWritten = await fs.writeFile( ENV.DATA_SOURCE.EU.DIFF_FILE, JSON.stringify(diffData, null, 4), 'utf8');
+    console.log("|    Saved missing entity/person from EU list     \u{1F4BE} \u{1F4BE}                      |");
+}
+
+function updateName(currentName, oName) {
+    function update(curName, NewPart) {
+        if (curName) {
+            curName += " " + NewPart;
+        } else {
+            curName += NewPart;
+        }
+        return curName;
+    }
+    if (oName.Name1) {
+        currentName = update(currentName, oName.Name1);
+    }
+    if (oName.Name2) {
+        currentName = update(currentName, oName.Name2);
+    }
+    if (oName.Name3) {
+        currentName = update(currentName, oName.Name3);
+    }
+    if (oName.Name4) {
+        currentName = update(currentName, oName.Name4);
+    }
+    if (oName.Name5) {
+        currentName = update(currentName, oName.Name5);
+    }
+    if (oName.Name6) {
+        currentName = update(currentName, oName.Name6);
+    }
+    return currentName;
 }
